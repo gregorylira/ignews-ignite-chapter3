949e4595743b19a87175a81798294680a5ad246b
gregorylira <gregory.lira007@gmail.com>
2022-03-11 15:14:37 -0300

    feat: Sistema de login funcional

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,19 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
+  const { data: session, status } = useSession();
+
+  return session ? (
     <button type="button" className={styles.signInButton}>
-      <img src="/images/Anonymous.jpeg" alt="ProfileImage" />
-      Gregory Filipe Lira da Silva
+      <img src={session.user.image} alt="ProfileImage" />
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
