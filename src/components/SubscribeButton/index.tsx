a25366bda8b7820f99b916016839de054ef32271
gregorylira <gregory.lira007@gmail.com>
2022-03-30 14:00:52 -0300

    🌟 feat: criando subscribe no stripe para pagamento

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,22 @@
 import styles from "./styles.module.scss";
+import { signIn, useSession } from "next-auth/react";
 
 export function SubscribeButton() {
+  const { data: session, status } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
