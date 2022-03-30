e636cc3c28842624562dba9f969449e4514fafda
gregorylira <gregory.lira007@gmail.com>
2022-03-30 14:28:54 -0300

    🌟 feat: funcionalidade de pagamento com o stripe + fix de duplicação removido

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,27 @@
 import styles from "./styles.module.scss";
 import { signIn, useSession } from "next-auth/react";
+import { api } from "../../service/api";
+import { getStripeJs } from "../../service/stripe-js";
 
 export function SubscribeButton() {
   const { data: session, status } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
     }
   }
 
